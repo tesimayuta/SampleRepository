b27ba1414bd8ec4b0183954af3f04dc271ac3520
ns2pole <spaceofstar2@gmail.com>
2024-07-16 19:08:27 +0900

    Example

---
+++ Example.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Example { // クラス変数の定義
+    public static int classVariable = 10;
+
+    public static void main(String[] args) {
+        // クラス変数にアクセスして出力
+        System.out.println("The value of Example.classVariable is: " + Example.classVariable);
+    }
+}
+
